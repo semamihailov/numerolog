6c29ff9b352736f61cd21f57141f5922306f6a9a
sema <semamihailov05@gmail.com>
2025-09-09 12:16:19 +0300

    save

--- js/main.js
+++ js/main.js
@@ -1,7 +1,14 @@
 "use strict";
 $(function () {
-    $(".mask").mask("99.99.9999");
+    $(".mask-date").mask("99.99.9999");
 
 
 
+    $(".calc-soul").click(function() {
+        $(".soul-img").css("display", "none");
+        $(".soul-wrapper").css("display", "block");
+    });
+
+
+    
 })
